02a8228e048a514fc59ce68513c5ca6b6169da61
Vinh Nguyen Le <vinh.nguyenlexuan@asnet.com.vn>
2015-02-27 20:32:02 +0700

    add test case

---
+++ spec/lib/pstatus_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+
+describe Pstatus do
+  it "should raise an error if the status_code is missing"
+
+  it "should raise an error if the status_code does not exist"
+
+  it "should raise an error if the status_message does not exist"
+
+  it "should raise an error if the status_message is missign."
+end
